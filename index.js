d7b5b8b357f3f7fffb3732d0304647d9960e5759
kalathuruTeja <tejakalathuru7883@gmail.com>
2023-01-25 16:26:47 +0530

    changes

---
+++ index.js
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Pricing Pages</title>
+</head>
+
+<body>
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots text-muted" viewBox="0 0 16 16">
+            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+        </svg>
+    </div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <h1 class="navbar-brand" href="#">
+                    Restart</h1>
+                <div>
+                    <h3>jeety</h3>
+                </div>
+                <div>
+                    <ul class="mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-dots ml-2" viewBox="0 0 16 16">
+                            <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                            <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
+                        </svg>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container text-center p-5 ">
+        <h3 class=" heading  ">Pick a base Plan :</h3>
+    </div>
+    <div class="container text-center ">
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class="col mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-header changes">
+                        <h2 class="text-muted ">Basic</h2>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted">A no-trils plane to meet most landiods requriments.</p>
+                        <h1 class="m-0 font-weight-normal text-muted">$12
+                            <small class="text-muted small">55/mo</small>
+                        </h1>
+                        <h6 class="text-muted mt-3">KEY FEATURES</h6>
+                        <ul class="text-muted list-unstyled mt-3 mb-4">
+                            <li>Everyday Belongings $10,000</li>
+                            <li>Protable Electronics $10,000</li>
+                            <li>Everyday Belongings $10,000</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card shadow-lg">
+                    <div class="card-header">
+                        <h1 class="color">Essential</h1>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted">our top-seller for the avearge renter </p>
+                        <h1 class="m-0 font-weight-normal color">$18
+                            <small class="text-muted small">55/mo</small>
+                        </h1>
+                        <h6 class="text-muted mt-3">KEY FEATURES</h6>
+                        <ul class="text-muted list-unstyled mt-3 mb-4">
+                            <li>Everyday Belongings <span>$10,000</span></li>
+                            <li>Protable Electronics<span> $10,00</span></li>
+                            <li>Everyday Belongings <span>$10,000</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h2 class="text-muted">Premium</h2>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-muted">The perect plane for those with few extra belongings</p>
+                        <h1 class="m-0 font-weight-normal text-muted">$33
+                            <small class="text-muted small">55/mo</small>
+                        </h1>
+                        <h6 class="text-muted mt-3">KEY FEATURES</h6>
+                        <ul class="text-muted list-unstyled mt-3 mb-4">
+                            <li>Everyday Belongings $40,000</li>
+                            <li>Protable Electronics $3,000</li>
+                            <li>Everyday Belongings $400,000</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary">Choose Plan</button>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
